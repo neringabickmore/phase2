1084078e8ab494b7a67e46205737115b0375bfb7
neringa <neringa.bickmore@gmail.com>
2023-07-23 22:43:42 +0100

    Add scroll up button with styling and js

--- script.js
+++ script.js
@@ -9,4 +9,50 @@ $(document).ready(function(){
     function changeBars(i) {
         i.classList.toggle("change");
     };
+
+    
 });
+
+const goUpBtn = document.getElementById("goUpBtn");
+  
+/**
+* When the user scrolls down 
+* 20px from the top of the document
+* show the button.
+*/
+window.onscroll = function() {
+    scrollFunction();
+};
+
+/**
+* Scroll function:
+* when the user scrolls the page
+* Otherwise, the button remains invisible. 
+* the button to go up appears. 
+*/
+const scrollFunction = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      goUpBtn.style.display = "block";
+    } else {
+      goUpBtn.style.display = "none";
+    }
+  };
+  
+/**
+* When the user clicks on the button
+* scroll to the top of the document
+*/
+const topFunction = () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+// Event Listener
+  
+$("#goUpBtn").click(function () {
+    topFunction();
+});
+
+
+  
+  
