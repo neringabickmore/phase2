97e29254d1fffde467a3d5498851a8173f17530f
neringa <neringa.bickmore@gmail.com>
2024-10-04 08:40:55 +0100

    Added basic modal with the form and functionality

--- script.js
+++ script.js
@@ -21,6 +21,55 @@ $(document).ready(function(){
     
 });
 
+// Modal
+
+window.onload = function() {
+  setTimeout(function() {
+    document.getElementById("offerModal").style.display = "block";
+  }, 5000); // 8 seconds delay
+
+  document.getElementById("closeModal").onclick = function() {
+    document.getElementById("offerModal").style.display = "none";
+  };
+
+  document.getElementById("emailForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    // Capture the current date and time in ISO format
+    var currentTimestamp = new Date().toISOString();
+    document.getElementById("timestamp").value = currentTimestamp; // Set the hidden input value
+
+    
+    // Get the email input
+    var email = document.getElementById("email").value;
+    // var currentTimestamp = document.getElementById("timestamp").value;
+
+    // Send data to Google Apps Script using a POST request
+    fetch('https://script.google.com/macros/s/AKfycbxHwwzEcBgLRncTswXz83rZkhUfjt7O1Mbixf8YAjSL7MTryrPNFvm-kxzk-TzoPHeOeg/exec', {
+      method: 'POST',
+      body: new URLSearchParams({
+        'email': email,
+        'timestamp': currentTimestamp
+      })
+    })
+    .then(response => response.text())
+    .then(result => {
+      alert(result);  // You can customize the success message here
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  });
+
+
+  // Close the modal if the user clicks outside the modal content
+  window.onclick = function(event) {
+    if (event.target == document.getElementById("offerModal")) {
+      document.getElementById("offerModal").style.display = "none";
+    }
+  };
+};
+
 // loads tooltips 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
